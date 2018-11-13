cf8975205f78dd1812a0d7a515196ead2c66dae1
tinglun <tinglun.lin1994@gmail.com>
2018-11-13 22:28:36 +0800

    upload mbagility web work

---
+++ MB/Agility/js/detail/main.js
@@ -0,0 +1,40 @@
+$(function(){
+
+// Control Set ------------
+    // down-payment-percent 頭款% 拉桿
+    $("#DPP-slider").slider({ 
+        range: "max",
+        min: 10, 
+        max: 30, 
+        step: 10,
+        value: 20 /* 設定 初始值 */
+
+    }).slider("pips", {
+        rest: "label",
+        suffix: "%"
+    });
+
+
+    // terms 分期 拉桿
+    $("#terms-slider").slider({ 
+        range: "max",
+        min: 24, 
+        max: 36, 
+        step: 12,
+        value: 36 /* 設定 初始值 */
+
+    }).slider("pips", {
+        rest: "label",
+        suffix: "期"
+    });
+
+// Circle Set ------------
+    $.getScript("js/detail/data.js");
+
+});
+
+
+
+
+
+
